d6816e0d42743da0a6248ddc8a6f1e76f0344d92
James Gibson <james-gibson@ntlworld.com>
2023-11-09 23:03:47 +0000

    new-grammar

--- tree-sitter-surql/grammar.js
+++ tree-sitter-surql/grammar.js
@@ -2,28 +2,40 @@ module.exports = grammar({
   name: "surql",
 
   rules: {
-    source_file: ($) => repeat($._statement),
+    source_file: ($) => repeat(seq($._statement, ";")),
 
-    _statement: ($) => choice($.crud_statement, $.definition_statement),
+    _statement: ($) =>
+      choice($._crud_statement, $._define_statement, $.invalid_statement),
 
-    crud_statement: ($) => seq(repeat1($._statement_parts), ";"),
-
-    _statement_parts: ($) =>
+    _crud_statement: ($) =>
       choice(
-        $.create_part,
-        $.update_part,
-        $.delete_part,
-        $.select_part,
-        $.from_part,
-        $.where_part,
-        $.content_part,
+        $.create_statement,
+        $.update_statement,
+        $.delete_statement,
+        $.select_statement,
       ),
 
-    _simple_name: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    identifier: ($) => $._simple_name,
-    function_name: ($) =>
-      seq($._simple_name, repeat(seq("::", $._simple_name))),
-    variable: ($) => seq("$", $.identifier),
+    create_statement: ($) => seq($.create, $.content, optional($.filter)),
+    update_statement: ($) => seq($.update, $.content, optional($.filter)),
+    delete_statement: ($) => seq($.delete, optional($.filter)),
+    select_statement: ($) => seq($.select, $.from, optional($.filter)),
+
+    create: ($) => seq("create", optional($.identifier)),
+    update: ($) => seq("update", optional($.identifier)),
+    delete: ($) => seq("delete", optional($.identifier)),
+    select: ($) => seq("select", optional($.varargs)),
+
+    from: ($) => seq("from", optional($.identifier)),
+    content: ($) => seq("content", optional($._value)),
+
+    where: ($) => seq("where", optional($._value)),
+    order_by: ($) => seq("order by", optional($.varargs)),
+    limit: ($) => seq("limit", optional($._value)),
+    skip: ($) => seq("skip", optional($._value)),
+
+    filter: ($) => repeat1(choice($.where, $.order_by, $.limit, $.skip)),
+
+    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
     string: ($) => /"[^"]*"/,
     integer: ($) => /[0-9]+/,
@@ -32,12 +44,25 @@ module.exports = grammar({
 
     array: ($) => seq("[", repeat(seq($._value, ",")), optional($._value), "]"),
     object_entry: ($) => seq($.identifier, ":", $._value),
+    invalid_object_entry: ($) =>
+      prec(-1, seq($.identifier, optional(":"), optional($._value))),
+
+    _object_entry: ($) =>
+      choice($.object_entry, prec(-1, $.invalid_object_entry)),
+
     object: ($) =>
-      seq("{", repeat(seq($.object_entry, ",")), optional($.object_entry), "}"),
+      seq(
+        "{",
+        repeat(seq($._object_entry, ",")),
+        optional($._object_entry),
+        "}",
+      ),
 
     varargs: ($) => seq($._value, repeat(seq(",", $._value))),
 
-    function_call: ($) => seq($.function_name, "(", optional($.varargs), ")"),
+    function_call: ($) => seq($.identifier, "(", optional($.varargs), ")"),
+
+    field_name: ($) => seq($.identifier, repeat(seq(".", $.identifier))),
 
     _value: ($) =>
       choice(
@@ -49,7 +74,6 @@ module.exports = grammar({
         $.object,
         $.identifier,
         $.function_call,
-        $._operation,
         $.property_access,
         $.variable,
       ),
@@ -67,98 +91,68 @@ module.exports = grammar({
         $.greater_than_or_equal_to_operation,
         $.less_than_or_equal_to_operation,
         $.and_operation,
-        $.or_operation,
       ),
 
-    property_access: ($) => prec.left(0, seq($._value, ".", $.identifier)),
-    division_operation: ($) => prec.left(-1, seq($._value, "/", $._value)),
-    multiplication_operation: ($) =>
-      prec.left(-1, seq($._value, "*", $._value)),
-    addition_operation: ($) => prec.left(-2, seq($._value, "+", $._value)),
-    subtraction_operation: ($) => prec.left(-2, seq($._value, "-", $._value)),
-    less_than_operation: ($) => prec.left(-3, seq($._value, "<", $._value)),
-    less_than_or_equal_to_operation: ($) =>
-      prec.left(-3, seq($._value, "<=", $._value)),
-    greater_than_operation: ($) => prec.left(-3, seq($._value, ">", $._value)),
+    addition_operation: ($) => prec.left(1, seq($._value, "+", $._value)),
+    subtraction_operation: ($) => prec.left(1, seq($._value, "-", $._value)),
+    multiplication_operation: ($) => prec.left(2, seq($._value, "*", $._value)),
+    division_operation: ($) => prec.left(2, seq($._value, "/", $._value)),
+    equality_operation: ($) => prec.left(3, seq($._value, "=", $._value)),
+    inequality_operation: ($) => prec.left(3, seq($._value, "!=", $._value)),
+    greater_than_operation: ($) => prec.left(3, seq($._value, ">", $._value)),
+    less_than_operation: ($) => prec.left(3, seq($._value, "<", $._value)),
     greater_than_or_equal_to_operation: ($) =>
-      prec.left(-3, seq($._value, ">=", $._value)),
-    inequality_operation: ($) => prec.left(-4, seq($._value, "!=", $._value)),
-    equality_operation: ($) => prec.left(-4, seq($._value, "=", $._value)),
-    or_operation: ($) => prec.left(-6, seq($._value, $.or_token, $._value)),
-    and_operation: ($) => prec.left(-7, seq($._value, $.and_token, $._value)),
+      prec.left(3, seq($._value, ">=", $._value)),
+    less_than_or_equal_to_operation: ($) =>
+      prec.left(3, seq($._value, "<=", $._value)),
+    and_operation: ($) => prec.left(4, seq($._value, "and", $._value)),
+    or_operation: ($) => prec.left(5, seq($._value, "or", $._value)),
+    not_operation: ($) => prec.left(6, seq("!", $._value)),
+    property_access: ($) => prec.left(7, seq($._value, ".", $.identifier)),
 
-    create_token: ($) => "create",
-    update_token: ($) => "update",
-    delete_token: ($) => "delete",
-    select_token: ($) => "select",
-    from_token: ($) => "from",
-    where_token: ($) => "where",
-    content_token: ($) => "content",
-    define_token: ($) => "define",
-    table_token: ($) => "table",
-    field_token: ($) => "field",
-    on_token: ($) => "on",
-    type_token: ($) => "type",
-    function_token: ($) => "function",
-    and_token: ($) => "and",
-    or_token: ($) => "or",
+    variable: ($) => seq("$", $.identifier),
 
-    delete_part: ($) => seq($.delete_token, optional($.identifier)),
-    create_part: ($) => seq($.create_token, optional($.identifier)),
-    content_part: ($) => seq($.content_token, optional($._value)),
-    select_part: ($) => seq($.select_token, optional($.varargs)),
-    from_part: ($) => seq($.from_token, optional($.identifier)),
-    where_part: ($) => seq($.where_token, optional($._value)),
-    update_part: ($) => seq($.update_token, optional($.identifier)),
+    _statement_parts: ($) =>
+      prec(
+        -1,
+        choice(
+          $.create,
+          $.update,
+          $.delete,
+          $.select,
+          $.from,
+          $.content,
+          $.where,
+          $.order_by,
+          $.limit,
+          $.skip,
+        ),
+      ),
 
-    definition_statement: ($) =>
+    invalid_statement: ($) => repeat1($._statement_parts),
+
+    _define_statement: ($) =>
       seq(
-        $.define_token,
-        choice(repeat($._define_part), $.function_definition_statement),
-        ";",
+        "define",
+        choice($.define_table_statement, $._define_field_statement),
       ),
 
-    _define_part: ($) =>
-      choice($.table_part, $.field_part, $.type_part, $.on_part),
+    define_table_statement: ($) => seq("table", optional($.identifier)),
+    define_field_statement: ($) => seq($.field, $.on, $.type),
+    invalid_define_field_statement: ($) =>
+      prec(-1, seq($.field, optional($.on), optional($.type))),
+    _define_field_statement: ($) =>
+      choice($.define_field_statement, $.invalid_define_field_statement),
 
-    type_name: ($) =>
-      choice(
-        "string",
-        "integer",
-        "float",
-        "boolean",
-        $.array_type,
-        $.optional_type,
-      ),
-    array_type: ($) => seq("array", "<", $.type_name, ">"),
-    optional_type: ($) => seq("optional", "<", $.type_name, ">"),
+    field: ($) => seq("field", optional($.field_name)),
+    on: ($) => seq("on", optional($.identifier)),
+    type: ($) => seq("type", optional($.type_identifier)),
 
-    table_part: ($) => seq($.table_token, optional($.identifier)),
-    field_part: ($) => seq($.field_token, optional($.identifier)),
-    on_part: ($) => seq($.on_token, optional($.identifier)),
-    type_part: ($) => seq($.type_token, optional($.type_name)),
+    type_identifier: ($) => choice($.identifier, $.generic_type),
 
-    function_name_part: ($) => seq($.function_token, optional($.function_name)),
+    type_name: ($) => $.identifier,
+    generic_type: ($) => seq($.identifier, "<", $.type_identifier, ">"),
 
-    function_param: ($) => seq($.variable, ":", $.type_name),
-
-    function_arguments_part: ($) =>
-      seq(
-        "(",
-        optional(seq($.function_param, repeat(seq(",", $.function_param)))),
-        ")",
-      ),
-
-    function_return_type_part: ($) => seq(":", $.type_name),
-
-    function_body_part: ($) => seq("{", repeat($._statement), "}"),
-
-    function_definition_statement: ($) =>
-      seq(
-        $.function_name_part,
-        optional($.function_arguments_part),
-        optional($.function_return_type_part),
-        optional($.function_body_part),
-      ),
+    table: ($) => seq("table", $.identifier),
   },
 });
