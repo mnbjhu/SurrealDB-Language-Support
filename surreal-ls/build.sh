6570f78fda71664567473af8314bec633ffedd66
James Gibson <james-gibson@ntlworld.com>
2023-11-14 19:38:30 +0000

    removing build files

--- surreal-ls/build.sh
+++ surreal-ls/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+(
+	cd ../tree-sitter-surql || exit
+	tree-sitter generate
+)
 cp -f ../tree-sitter-surql/src/parser.c ./bindings/parser.c || exit 1
 cp -f ../tree-sitter-surql/src/tree_sitter/parser.h ./bindings/tree_sitter/parser.h || exit 1
 
